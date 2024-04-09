25e08b960b8936a2e788b3d6e1d672ce11e82773
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-04-09 00:39:15 +0000

    fixed constion apos

--- app/conditions/page.tsx
+++ app/conditions/page.tsx
@@ -10,28 +10,29 @@ export default function TermsAndConditions() {
           <div className="text-4xl font-bold mb-10">Terms and conditions</div>
           <div className="font-bold text-xl mb-5">Last updated : March 19, 2024</div>
           <div>
-            We, Charge Me Now LLC ('C M N', 'Company', 'we', 'us', or 'our'), are a registered
-            company located in Broward County, Coral Springs, Florida. We manage the mobile
-            application CMN (Charge Me Now), as well as any other associated products and services
-            connected to these legal terms (referred to as the 'Legal Terms') that have been
-            selected by you or your electric vehicle dealer (together known as the 'Services'). You
-            can reach us via phone at (954) 417-7696, email at help@gochargemenow.com These Legal
-            Terms form a legally binding agreement between you, whether individually or on behalf of
-            an entity ('you'), and Charge Me Now LLC, regarding your use of the Services. By
-            accessing the Services, you acknowledge that you have read, understood, and agreed to
-            comply with all these Legal Terms. IF YOU DISAGREE WITH ANY PART OF THESE LEGAL TERMS,
-            YOU ARE NOT ALLOWED TO USE THE SERVICES, AND YOU MUST STOP USING THEM IMMEDIATELY.
-            Additional terms and conditions or documents that may be published in the Services
-            periodically are explicitly included here by reference. We reserve the right, at our
-            sole discretion, to amend or modify these Legal Terms at any time and for any reason.
-            Notification of any changes will be provided by updating the 'Last updated' date of
-            these Legal Terms. It is your responsibility to regularly review these Legal Terms to
-            stay informed of any updates. By continuing to use the Services after the posting of any
-            revised Legal Terms, you will be considered to have accepted the changes. All users who
-            are minors in their jurisdiction (usually under the age of 18) must obtain permission
-            from, and be directly supervised by, their parents or guardians to use the Services. If
-            you are a minor, your parent or guardian must read and agree to these Legal Terms before
-            you can use the Services.
+            We, Charge Me Now LLC (&apos;C M N&apos;, &apos;Company&apos;, &apos;we&apos;,
+            &apos;us&apos;, or &apos;our&apos;), are a registered company located in Broward County,
+            Coral Springs, Florida. We manage the mobile application CMN (Charge Me Now), as well as
+            any other associated products and services connected to these legal terms (referred to
+            as the &apos;Legal Terms&apos;) that have been selected by you or your electric vehicle
+            dealer (together known as the &apos;Services&apos;). You can reach us via phone at (954)
+            417-7696, email at help@gochargemenow.com These Legal Terms form a legally binding
+            agreement between you, whether individually or on behalf of an entity (&apos;you&apos;),
+            and Charge Me Now LLC, regarding your use of the Services. By accessing the Services,
+            you acknowledge that you have read, understood, and agreed to comply with all these
+            Legal Terms. IF YOU DISAGREE WITH ANY PART OF THESE LEGAL TERMS, YOU ARE NOT ALLOWED TO
+            USE THE SERVICES, AND YOU MUST STOP USING THEM IMMEDIATELY. Additional terms and
+            conditions or documents that may be published in the Services periodically are
+            explicitly included here by reference. We reserve the right, at our sole discretion, to
+            amend or modify these Legal Terms at any time and for any reason. Notification of any
+            changes will be provided by updating the &apos;Last updated&apos; date of these Legal
+            Terms. It is your responsibility to regularly review these Legal Terms to stay informed
+            of any updates. By continuing to use the Services after the posting of any revised Legal
+            Terms, you will be considered to have accepted the changes. All users who are minors in
+            their jurisdiction (usually under the age of 18) must obtain permission from, and be
+            directly supervised by, their parents or guardians to use the Services. If you are a
+            minor, your parent or guardian must read and agree to these Legal Terms before you can
+            use the Services.
             <div>1. Acceptance of Terms:</div>
             <div>
               By using the Services provided by Charge Me Now, you agree to be bound by these Terms
