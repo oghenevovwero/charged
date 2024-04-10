0ed57df03897b5b51edafaa14d0eb663cde1565d
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-04-10 00:36:32 +0000

    blurred image

--- components/_home/lagos-map.tsx
+++ components/_home/lagos-map.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import chargeImg from "@/public/charger.png";
 import mapImg from "@/public/lagos.png";
+import Padding from "../padding";
 
 export default function LagosMap() {
   return (
@@ -14,8 +15,14 @@ export default function LagosMap() {
         sizes="100vw"
         style={{ objectFit: "cover" }}
       />
-      <div className="absolute inset-0 w-screen h-full flex justify-center items-center">
-        <Image src={chargeImg} className="h-full mix-blend-color-burn" alt="charge station" />
+      <div className="absolute inset-0 w-screen h-full">
+        <Padding className="flex justify-end h-full">
+          <Image
+            src={chargeImg}
+            className="h-full mix-blend-multiply blur-[3px]"
+            alt="charge station"
+          />
+        </Padding>
       </div>
     </div>
   );
