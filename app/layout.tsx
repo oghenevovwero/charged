6a97000f8b8fe76368865c7aa1d23a270b83dc03
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-04-10 14:17:06 +0000

    changed font style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,78 @@
+import local from 'next/font/local';
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
 const inter = Open_Sans({ subsets: ["latin"] });
+
+const openSauceSans = local({
+  src: [
+    {
+      path: '../public/fonts/OpenSauceSans-Black.ttf',
+      weight: '400',
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-BlackItalic.ttf',
+      weight: '400',
+      style: "italic",
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-Bold.ttf',
+      weight: '700',
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-BoldItalic.ttf',
+      weight: '700',
+      style: "italic",
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-ExtraBold.ttf',
+      weight: '800',
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-ExtraBoldItalic.ttf',
+      weight: '800',
+      style: "italic",
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-Italic.ttf',
+      weight: '400',
+      style: "italic",
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-Light.ttf',
+      weight: '300',
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-LightItalic.ttf',
+      weight: '300',
+      style: "italic"
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-Medium.ttf',
+      weight: '500',
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-MediumItalic.ttf',
+      weight: '500',
+      style: "italic"
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-Regular.ttf',
+      weight: '400',
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-SemiBold.ttf',
+      weight: '600',
+    },
+    {
+      path: '../public/fonts/OpenSauceSans-SemiBoldItalic.ttf',
+      weight: '600',
+      style: "italic"
+    },
+  ],
+  variable: '--font-open-sauce-sans',
+});
 
 export const metadata: Metadata = {
   title: "Get charged now!",
@@ -16,7 +86,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${openSauceSans.variable} ${inter.className}`}>{children}</body>
     </html>
   );
 }
