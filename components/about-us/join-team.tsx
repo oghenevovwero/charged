3f942eac6e5d8248554f6fc719323a5b0cc30f70
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-04-10 19:58:21 +0000

    fixed join team image height

--- components/about-us/join-team.tsx
+++ components/about-us/join-team.tsx
@@ -6,7 +6,7 @@ import Container from "../container";
 export default function JoinTheTeam() {
   return (
     <Container className="flex flex-col gap-10 items-center md:flex-row bg-[#F6F8F7]  text-black">
-      <div className="flex-1">
+      <div className="flex-1 lg:pl-28 lg:pr-12">
         <div className="text-4xl font-bold">Come join the team</div>
         <div className="my-6">
           Work with some of the best global talent to push a sustainable and convenient charging
@@ -22,7 +22,7 @@ export default function JoinTheTeam() {
         </div>
       </div>
       <div className="flex-1 relative">
-        <Image alt="members" className="rounded-md md:rounded-r-lg" src={memberImg} />
+        <Image alt="members" height={478} className="max-md:rounded-md md:rounded-r-3xl" src={memberImg} />
       </div>
     </Container>
   );
