f8555be5e4af67776771fe7d760f23738913cbd5
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-04-10 22:11:09 +0000

    home page banner

--- components/_home/home-banner.tsx
+++ components/_home/home-banner.tsx
@@ -19,20 +19,16 @@ export default function HomeBanner() {
       />
       <div className="absolute inset-0 w-screen h-full flex justify-start items-center">
         <Container className="flex flex-col gap-5 items-start text-white">
-          <div className="text-6xl font-semibold">Get charged, anytime, anywhere</div>
-          <div className="text-xl my-5 font-light sm:w-[60%] md:w-1/2">
-            Introducing a revolutionary charging service that brings the power to you
+          <div className="sm:w-[60%]">
+            <div className="text-6xl font-semibold">Get charged, anytime, anywhere</div>
+            <div className="text-xl mt-10 mb-8 font-light">
+              Introducing a revolutionary charging service that brings the power to you
+            </div>
           </div>
           <div className="flex gap-3 justify-center items-center">
             <div className="p-2 cursor-pointer active:opacity-50 hover:opacity-75 rounded-lg text-white border border-gray-500 flex gap-1 items-center">
               <div>
-                <Image
-                  src={appleImg}
-                  width={20}
-                  height={20}
-                  alt="Get on app store"
-                  quality={100}
-                />
+                <Image src={appleImg} width={20} height={20} alt="Get on app store" quality={100} />
               </div>
               <div>
                 <div className="text-[8px]">COMING SOON ON</div>
@@ -41,13 +37,7 @@ export default function HomeBanner() {
             </div>
             <div className="p-2 rounded-lg cursor-pointer hover:opacity-75 active:opacity-50 text-white border border-gray-500 flex gap-1 items-center">
               <div>
-                <Image
-                  src={playImg}
-                  width={20}
-                  height={20}
-                  alt="Get on play store"
-                  quality={100}
-                />
+                <Image src={playImg} width={20} height={20} alt="Get on play store" quality={100} />
               </div>
               <div>
                 <div className="text-[8px]">COMING SOON ON</div>
