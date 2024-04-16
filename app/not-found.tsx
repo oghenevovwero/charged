d53de7f466c301190aa458e1ca161d8fb27e85d1
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-04-16 08:14:23 +0000

    updated .gitignore

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,14 @@
-"use client";
-
 import ExportedImage from "next-image-export-optimizer";
 import logoImg from "@/public/images/the-logo.png";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Charged404() {
-  const pathName = usePathname();
-
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-[#1A202B]">
       <ExportedImage src={logoImg} alt="our logo" className="animate-pulse" />
       <header className="text-5xl font-bold my-2">Sorry!</header>
-      <p>{pathName} was not found</p>
+      <p>requested page was not found</p>
 
       <Link
         href={"/"}
