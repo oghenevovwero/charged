e14ea55c6a2750f8fbcf365a79407aeef50d68c1
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-05-03 16:47:07 +0000

    fixed waitlist

--- components/home/join-waitlist/join-waitlist.tsx
+++ components/home/join-waitlist/join-waitlist.tsx
@@ -7,7 +7,7 @@ export default function WaitList() {
   return (
     <Container className="flex items-center justify-center bg-[#F6F8F7] text-black">
       <div className="flex flex-col md:h-96 md:flex-row">
-        <div className="flex flex-1 flex-col justify-center rounded-3xl bg-[#54645D] p-6 text-white sm:p-20 md:rounded-br-none md:rounded-tr-none md:p-20">
+        <div className="flex flex-1 flex-col justify-center rounded-3xl bg-[#54645D] p-6 text-white sm:p-20 lg:rounded-br-none lg:rounded-tr-none md:p-20">
           <div className="text-4xl font-bold">Join the WaitList</div>
           <div className="mb-14 mt-4 text-lg">
             Be among the first to experience the future of EV charging. Enter
@@ -17,7 +17,7 @@ export default function WaitList() {
             <WaitlistForm />
           </div>
         </div>
-        <div className="h-96 w-full flex-1 max-md:hidden">
+        <div className="h-96 w-full flex-1 max-lg:hidden">
           <ExportedImage
             className="h-full w-full rounded-r-3xl"
             src={plugImg}
